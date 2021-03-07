b5b97afca6939261c49f622716c003911e3f2966
vladimir <vladi2909@outlook.com>
2021-03-07 19:35:29 +0300

    feat: add active btns

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fullscreen = document.querySelector('.fullscreen').addEventListener('click', toggleScreen);
 
+const btnNotes = document.querySelector('.btn-notes');
+const btnLetters = document.querySelector('.btn-letters');
+btnNotes.addEventListener('click', activeBtns);
+btnLetters.addEventListener('click', activeBtns);
+
+const keys = document.querySelectorAll('.piano-key');
+
 function toggleScreen() {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
@@ -7,5 +14,17 @@ function toggleScreen() {
         if (document.fullscreenEnabled) {
             document.exitFullscreen();
         }
+    }
+}
+
+function activeBtns(e) {
+    if (e.target.classList.contains('btn-letters') && !e.target.classList.contains('btn-active')) {
+        btnLetters.classList.add('btn-active');
+        btnNotes.classList.remove('btn-active');
+        keys.forEach(key => key.classList.add('key-active'));
+    } else if (e.target.classList.contains('btn-notes') && !e.target.classList.contains('btn-active')) {
+        btnNotes.classList.add('btn-active');
+        btnLetters.classList.remove('btn-active');
+        keys.forEach(key => key.classList.remove('key-active'));
     }
 }
